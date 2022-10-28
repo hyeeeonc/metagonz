dc7203b9229d31714a8ab2faf0d512c21f859558
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-10-29 04:09:14 +0900

    audioProvider 여러가지 추가

--- src/contexts/AudioProvider.tsx
+++ src/contexts/AudioProvider.tsx
@@ -6,12 +6,20 @@ import {
   ComponentProps,
   FC,
   PropsWithChildren,
+  useEffect,
+  useCallback,
 } from 'react'
 
 type AudioContextValue = {
   src: string
   audioRef: React.RefObject<HTMLAudioElement>
   setAudio: (src: string) => void
+  isPlaying: boolean
+  playAudio: () => void
+  pauseAudio: () => void
+  setDefaultAudio: () => void
+  title: string
+  setTitle: React.Dispatch<React.SetStateAction<string>>
 }
 
 export const AudioContext = createContext<AudioContextValue>(
@@ -20,7 +28,20 @@ export const AudioContext = createContext<AudioContextValue>(
 
 const AudioProvider = ({ children }: ComponentProps<FC<PropsWithChildren>>) => {
   const [src, setSrc] = useState<string>('./static/audios/emaj01.mp3')
+  const [title, setTitle] = useState<string>('Default Audio')
   const audioRef = useRef<HTMLAudioElement>(null)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.onpause = () => {
+        setIsPlaying(false)
+      }
+      audioRef.current.onplay = () => {
+        setIsPlaying(true)
+      }
+    }
+  }, [])
 
   const setAudio = (src: string) => {
     setSrc(src)
@@ -34,11 +55,33 @@ const AudioProvider = ({ children }: ComponentProps<FC<PropsWithChildren>>) => {
     }
   }
 
+  const playAudio = useCallback(() => {
+    if (!isPlaying) {
+      audioRef.current?.play()
+    }
+  }, [isPlaying])
+
+  const pauseAudio = useCallback(() => {
+    if (isPlaying) {
+      audioRef.current?.pause()
+    }
+  }, [isPlaying])
+
+  const setDefaultAudio = () => {
+    setAudio('./static/audios/emaj01.mp3')
+  }
+
   const value = useMemo(
     () => ({
       src,
       audioRef,
       setAudio,
+      isPlaying,
+      playAudio,
+      pauseAudio,
+      setDefaultAudio,
+      title,
+      setTitle,
     }),
     [src, audioRef],
   )
